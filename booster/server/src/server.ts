aa04a1406cada6129ffa9876ea0665d046364049
Willian Genero <willian-b-g@hotmail.com>
2020-06-05 17:23:48 -0300

    Conhecimentos básicos rotas, recursos, métodos e parâmetros

--- booster/server/src/server.ts
+++ booster/server/src/server.ts
@@ -1,15 +1,52 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
-app.get('/users', (request, response) => {
-  console.log('Listagem de usuários');
+app.use(express.json())
+// Rota: Endereço completo da requisição
+// Recurso: Qual entidade estamos acessando do sistema
 
-  response.json([
-    'Diego',
-    'Willian',
-    'Duda'
-  ]);
+// Métodos:
+// GET: Buscar uma ou mais informações do back-end
+// POST: Criar uma nova informação no back-end
+// PUT: Atualiza uma informação existente no backe-end
+// DELETE: Deletar uma informação do back-end
+
+// Request param: Parâmetros que vem na própria rota que identificam um recurso
+// Query param: Parâmetros que vem na própria rota, geralmente opcionais para filtros, paginação, ...
+// Request body: Parâmetros para criação/atualização de informações
+
+const users = [
+  'Diego',
+  'Willian',
+  'Duda'
+];
+
+app.get('/users', (request, response) => {
+  const search = String(request.query.search);
+
+  const filteredUsers = search ? users.filter(user => user.includes(search)) : users;
+
+  return response.json(filteredUsers);
+});
+
+app.get('/users/:id', (request, response) => {
+  const id = Number(request.params.id);
+
+  const user = users[id];
+
+  return response.json(user)
+});
+
+app.post('/users', (request, response) => {
+  const data = request.body;
+
+  const user = {
+    name: data.name,
+    email: data.email,
+  };
+
+  return response.json(user);
 });
 
 app.listen(3333);
